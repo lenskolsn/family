7b9f9d2f7f3748bc2e7e65357a9fea796baa358e
lnam6507 <lnam6507@gmail.com>
2022-05-30 19:56:12 +0700

    update

---
+++ resources/views/components/trangchu.blade.php
@@ -0,0 +1,183 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>{{$attributes['title']}}</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8" />
+    <link rel="icon" href="https://thumbs.dreamstime.com/b/flat-house-icon-vector-69856515.jpg" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+
+    <!-- Bootstrap CSS v5.0.2 -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous" />
+    <link rel="stylesheet" href="style.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" />
+</head>
+
+<body>
+    <!-- Top -->
+    <div class="top">
+        <div class="container">
+            <div class="row p-1">
+                <div class="col-md-6 text-light">
+                    <i class="fas fa-phone"></i>
+                    <span>0819817373</span>
+                    <i class="fas fa-phone"></i>
+                    <span>0354632349 (Nam Lê)</span>
+                </div>
+                <div class="col-md-6 text-light text-end">
+                    <span>Liên hệ:</span>
+                    <span class="ms-2"><a href="https://www.facebook.com/LenskoLSN/" class="text-light"><i
+                                class="fab fa-facebook"></i></a></span>
+                    <span class="ms-2"><a href="" class="text-light"><i
+                                class="fas fa-envelope"></i></a></span>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Header -->
+    <header>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3">
+                    <div class="header_logo">
+                        <img src="http://maivangtet.com/wp-content/uploads/2021/10/maivangtet-300x100-1.png" alt="" />
+                    </div>
+                </div>
+                <div class="col-md-5">
+                    <div class="header_search">
+                        <select name="">
+                            <option value="">All</option>
+                            <option value="">Mai vàng chậu</option>
+                            <option value="">Mai vàng cho thuê</option>
+                            <option value="">Mai vườn</option>
+                            <option value="">Mai vàng giống</option>
+                        </select>
+                        <input type="text" />
+                        <button><i class="fas fa-search"></i></button>
+                    </div>
+                </div>
+                <div class="col-md-3 d-flex justify-content-end">
+                    <div class="header_login">
+                        <a href="" class="text-dark">ĐĂNG NHẬP</a>
+                    </div>
+                    <div class="header_cart ms-2">
+                        <a href="">GIỎ HÀNG <i class="fas fa-shopping-cart"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-white border border-1">
+        <div class="container">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
+                aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse d-flex justify-content-between" id="navbarNavDropdown">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" href="">Trang chủ</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="">Giới thiệu</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="" id="navbarDropdownMenuLink" role="button"
+                            data-bs-toggle="dropdown" aria-expanded="false">
+                            Sản phẩm
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                            <li><a class="dropdown-item" href="">Mai Vàng cho thuê</a></li>
+                            <li><a class="dropdown-item" href="">Mai Vàng chậu</a></li>
+                            <li><a class="dropdown-item" href="">Mai Vườn</a></li>
+                            <li><a class="dropdown-item" href="">Mai Vàng giống</a></li>
+                            <li>
+                                <a class="dropdown-item" href="">Phân. thuốc cho cây Mai</a>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+                <div class="contact">
+                    <span style="background: rgb(61, 184, 75); padding: 10px 15px; border-radius: 16px;"><a href=""
+                            class="text-light">0354632349 (Nam Lê)</a></span>
+                </div>
+            </div>
+        </div>
+    </nav>
+    <!-- Category and Banner -->
+    <section id="">
+        <div class="container">
+            <div class="row py-2">
+                <div class="col-md-3">
+                    <div class="category">
+                        <ul class="list-group">
+                            <li id="active" class="list-group-item" aria-current="true">
+                                <i class="fas fa-bars"></i> Danh mục sản phẩm
+                            </li>
+                            <li class="list-group-item"><a href="">Sản phẩm mới nhất</a></li>
+                            <li class="list-group-item"><a href="">Mai Vàng cho Thuê</a></li>
+                            <li class="list-group-item"><a href="">Mai Vàng Dáng BonSai</a></li>
+                            <li class="list-group-item"><a href="">Mai Vàng Chậu</a></li>
+                            <li class="list-group-item"><a href="">Mai Vàng giống</a></li>
+                            <li class="list-group-item"><a href="">Phân, Thuốc cho cây Mai</a></li>
+                            <li class="list-group-item"><a href="">Dự án và đề tài Mai Vàng</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-md-9">
+                    <div class="banner">
+                        <div id="carouselExampleDark" class="carousel carousel-light slide" data-bs-ride="carousel">
+                            <div class="carousel-indicators">
+                                <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0"
+                                    class="active" aria-current="true" aria-label="Slide 1"></button>
+                                <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1"
+                                    aria-label="Slide 2"></button>
+                                <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2"
+                                    aria-label="Slide 3"></button>
+                            </div>
+                            <div class="carousel-inner">
+                                <div class="carousel-item active" data-bs-interval="10000">
+                                    <img src="https://maivangtet.com/wp-content/uploads/2021/10/mai-vang-tet-1536x590.jpg"
+                                        class="d-block w-100" alt="...">
+                                </div>
+                                <div class="carousel-item" data-bs-interval="2000">
+                                    <img src="https://maivangtet.com/wp-content/uploads/2021/10/panel-mai-vang-tet-binh-loi-1536x590.jpg"
+                                        class="d-block w-100" alt="...">
+                                </div>
+                            </div>
+                            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark"
+                                data-bs-slide="prev">
+                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                <span class="visually-hidden">Previous</span>
+                            </button>
+                            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark"
+                                data-bs-slide="next">
+                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                <span class="visually-hidden">Next</span>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <div class="new_product bg-light">
+        <div class="container">
+            <div class="row py-3">
+                <h4>SẢN PHẨM MỚI NHẤT</h4>
+            </div>
+        </div>
+    </div>
+    <!-- Bootstrap JavaScript Libraries -->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"
+        integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"
+        integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
