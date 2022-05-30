c9efca042bc9543f184eb37ca41d4449712ebeef
lnam6507 <lnam6507@gmail.com>
2022-05-30 20:01:00 +0700

    update

--- resources/views/components/trangchu.blade.php
+++ resources/views/components/trangchu.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 
 <head>
-    <title>{{$attributes['title']}}</title>
+    <title>{{ $attributes['title'] }}</title>
     <!-- Required meta tags -->
     <meta charset="utf-8" />
     <link rel="icon" href="https://thumbs.dreamstime.com/b/flat-house-icon-vector-69856515.jpg" />
@@ -11,7 +11,7 @@
     <!-- Bootstrap CSS v5.0.2 -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous" />
-    <link rel="stylesheet" href="style.css" />
+    <link rel="stylesheet" href="/css/style.css" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" />
 </head>
 
